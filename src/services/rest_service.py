3e811e427def17c48fda8f70cfe75e1b3782b337
victor <victor@pop-os.localdomain>
2021-08-26 23:21:42 -0300

    implement unauthorized rest response

--- src/services/rest_service.py
+++ src/services/rest_service.py
@@ -6,3 +6,10 @@ def bad_request(exception: BaseException):
         status_code=status.HTTP_400_BAD_REQUEST,
         detail="Error: {}".format(exception)
     )
+
+
+def unauthorized():
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials"
+    )
